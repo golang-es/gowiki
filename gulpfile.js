8dd317bf67a5767895fecb71260ec0bd7862bded
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2016-08-27 05:23:18 -0500

    Se agregó autoprefixer.
    En caso de necesitarse en un futuro, por el momento no tiene efecto.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 'use strict';
 
-var gulp        = require("gulp");
-var sass        = require("gulp-sass");
-var sourcemaps  = require('gulp-sourcemaps');
+var gulp         = require("gulp");
+var sass         = require("gulp-sass");
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
 
     return gulp.src('./resources/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/css'));
 });
